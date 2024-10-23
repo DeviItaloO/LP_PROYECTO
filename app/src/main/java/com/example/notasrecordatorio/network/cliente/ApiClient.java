cdd5d787aafb09a5d09fd789630719518c49cc57
DeviItaloO <italoespinoza1529@gmail.com>
2024-10-23 01:39:55 -0500

    Proyecto de Notas de Recordatorio ApiNotaRecordatorio implementacion crud usuario

--- app/src/main/java/com/example/notasrecordatorio/network/cliente/ApiClient.java
+++ app/src/main/java/com/example/notasrecordatorio/network/cliente/ApiClient.java
@@ -11,15 +11,14 @@ public class ApiClient {
         String baseUrl;
 
         if (baseUrlEnum.getValue() == 1) {
-            baseUrl = "http://MI_IP:8080/usuario/";
+            baseUrl = "http://MI_IP:8080/usuario/"; /* coloca tu ip */
         } else if (baseUrlEnum.getValue() == 2) {
-            baseUrl = "http://MI_IP:8080/notas/";
+            baseUrl = "http://MI_IP:8080/notas/"; /* coloca tu ip */
         } else if (baseUrlEnum.getValue() == 3) {
-            baseUrl = "http://MI_IP:8080/categoria/";
+            baseUrl = "http://MI_IP:8080/categoria/"; /* coloca tu ip */
         } else {
             throw new IllegalArgumentException("La URL base no es válida: " + baseUrlEnum);
         }
-
         return new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
